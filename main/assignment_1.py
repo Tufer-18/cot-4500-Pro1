e9f1060cc2dfcdf6b9799df03df7316de7369215
Tufer-18 <pr557116@ucf.edu>
2025-01-31 18:21:50 -0500

    Update assignment_1.py

--- main/assignment_1.py
+++ main/assignment_1.py
@@ -1,1 +1,19 @@
+import math
 
+def approximation(x0, tol = 0.000001)
+  x = x0
+  diff = x0
+  iter = 0
+
+  print("f{iter} : {x}")
+
+  while diff >= tol:
+    iter += 1
+    y = x
+    x = (x/2) + (1/x)
+    print("f{iter} : {x}")
+    diff = abs(x-y)
+  print(f"\nConvergence after {iter} iterations")
+  return x
+
+print(approximation(1.5))
