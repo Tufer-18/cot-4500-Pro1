6f7e27267f7cf2ddc0df96be19cda7674eaf8f12
Tufer-18 <pr557116@ucf.edu>
2025-01-31 18:28:44 -0500

    Update assignment_1.py
    Moved f's to correct spot.

--- main/assignment_1.py
+++ main/assignment_1.py
@@ -1,17 +1,17 @@
 import math
 
-def approximation(x0, tol = 0.000001)
+def approximation(x0, tol = 0.000001):
   x = x0
   diff = x0
   iter = 0
 
-  print("f{iter} : {x}")
+  print(f"{iter} : {x}")
 
   while diff >= tol:
     iter += 1
     y = x
     x = (x/2) + (1/x)
-    print("f{iter} : {x}")
+    print(f"{iter} : {x}")
     diff = abs(x-y)
   print(f"\nConvergence after {iter} iterations")
   return x
